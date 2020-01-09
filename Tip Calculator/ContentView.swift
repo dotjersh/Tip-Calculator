bc27452f725b9b3cd19ab61fa9d5a2f48a772075
John Slomka <jdslomka@icloud.com>
2020-01-09 12:28:01 -0500

    Initial Commit

---
+++ Tip Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by John Slomka on 2020-01-09.
+//  Copyright © 2020 John Slomka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
